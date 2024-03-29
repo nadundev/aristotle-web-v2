f1af4b1fc3b545c1b7a4e0a0e273e184d58e4627
“nadundev” <nadunnissankauiux@gmail.com>
2024-03-29 13:59:46 +0530

    improved some animations

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,31 +1,28 @@
 "use client";
 
 import React from "react";
-import ButtonAnimated from "./ui/buttonAnimated";
-import { CardStack } from "../components/ui/card-stack";
-import { cn } from "@/app/utils/cn";
 import screenshot from "../assets/images/app-screenshot.png";
 
 const Hero = () => {
   return (
     <section className="relative w-full bg-black">
       <div className="relative isolate">
-        <div
+        {/* <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
         >
           <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            className="animated-clip-path relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#c8c1ba] to-[#c3a37e] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           ></div>
-        </div>
+        </div> */}
         <div className="py-24 sm:py-32 lg:pb-40">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl text-center">
-              <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl leading-[46px] md:leading-[70px]">
+              <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl leading-[46px] md:leading-[70px] gradient-text">
                 Stay on top of your pipeline. Google alerts on steroids for
                 sales.
               </h1>
@@ -43,27 +40,29 @@ const Hero = () => {
                 </button>
               </div>
             </div>
-            <img
-              src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-              alt="App screenshot"
-              width="2432"
-              height="1442"
-              className="mt-16 rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10 sm:mt-24"
-            />
+            <div className="box">
+              <img
+                src={screenshot.src}
+                alt="App screenshot"
+                width="2432"
+                height="1442"
+                className="mt-16 sm:mt-24"
+              />
+            </div>
           </div>
         </div>
-        <div
+        {/* <div
           className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
           aria-hidden="true"
         >
           <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            className="animated-clip-path relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#c8c1ba] to-[#c3a37e] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           ></div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
